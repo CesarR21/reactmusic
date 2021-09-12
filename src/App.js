e5690eb55a0ee8da094ac0c65f19edaa8bde3db0
unknown <Bizcanna21@icloud.com>
2021-09-12 14:22:04 -0500

    upated react files cread css and table data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
-}
+const App =() => {
+    return <div className="app-container">
+        <table>
+        <thead>
+        <tr>
+            <th>Song</th>
+            <th>Title</th>
+            <th>Artist</th>
+            <th>Album</th>
+            <th>Genre</th>
+            <th>Release_Date</th>
+        </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Candy Shop</td>
+                <td>Candy Shop</td>
+                <td>50 Cent</td>
+                <td>Get Rich or Die Trying</td>
+                <td>Hip Hop</td>
+                <td>june 2008</td>
+            </tr>
+        </tbody>
+        </table>    
+        </div>;
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 
-const jsxElement = <h1>Music Library</h1>
 
-ReactDOM.render(jsxElement,document.getElementById('root'));
 
-  
+
+
