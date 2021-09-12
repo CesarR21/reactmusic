739db0d6b8c251ed910427284042d80d564c26ee
unknown <Bizcanna21@icloud.com>
2021-09-12 16:27:29 -0500

    displyed song title data created delete and update buttons,created components

---
+++ src/components/ReadOnly.js
@@ -0,0 +1,28 @@
+
+import React from "react";
+
+const ReadOnly = ({ song, handleEditClick, handleDeleteClick }) => {
+  return (
+    <tr>
+      <td>{song.song}</td>
+      <td>{song.title}</td>
+      <td>{song.album}</td>
+      <td>{song.artist}</td>
+      <td>{song.genre}</td>
+      <td>{song.release_date}</td>
+      
+      <td>
+        <button
+          type="button"
+          onClick={(event) => handleEditClick(event, song)}
+        >
+          Edit
+        </button>
+        <button type="button" onClick={() => handleDeleteClick(song.id)}>
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
+};
+export default ReadOnly;
